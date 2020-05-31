3c4cf1d985ecf233604251476be0b8f052c6c50e
Jason Hao <jasonhao@jasons-air.lan>
2020-05-31 13:07:26 -0700

    Finished basic caesar cipher

--- CaesarCipher/src/caesarCipher/caesarCipher.java
+++ CaesarCipher/src/caesarCipher/caesarCipher.java
@@ -1,10 +1,51 @@
 package caesarCipher;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class caesarCipher {
 	
-	public static void main(String [] args) {
-		System.out.println("Hello world");
-		System.out.println("Hello");
+	public static String encrypt(String text, int num) {
+		StringBuffer encryptedText = new StringBuffer();
+		text.toLowerCase();
+		for(int i = 0; i < text.length(); i++) {
+			if(text.charAt(i) == 32) {
+				continue;
+			}
+			if(Character.isUpperCase(text.charAt(i))) {
+				char ch = (char)(((int)text.charAt(i) + num - 65) % 26 + 65);
+				encryptedText.append(ch);
+			} else {
+				char ch = (char)(((int)text.charAt(i) + num - 97) % 26 + 97);
+				encryptedText.append(ch);
+			}
+		}
+		return encryptedText.toString();
 	}
-
+	
+	public static void main(String [] args) {
+		Scanner scan = new Scanner(System.in);
+		String text;
+		int shift;
+		while (true) {
+			System.out.print("Enter the text: ");
+			text = scan.nextLine();
+			if(!text.equals("")) {
+				break;
+			} else {
+				System.out.println("Please enter text");
+			}
+		}
+		while (true) {
+			System.out.print("Enter shift: ");
+			try {
+				shift = scan.nextInt();
+				break;
+			} catch (Exception e) {
+				System.out.println("Please enter correct shift value");
+			}
+		}
+		System.out.println("\nEncrypted text: " + encrypt(text, shift));
+		scan.close();
+	}
 }
